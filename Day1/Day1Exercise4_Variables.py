c82f2af672598aa5e022317ae3705a873d05b832
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-22 11:52:01 -0800

    Add Day1Exercise4

---
+++ Day1/Day1Exercise4_Variables.py
@@ -0,0 +1,15 @@
+# Switch values stored in variables
+# Don't change the code below
+a = input("a: ")
+b = input("b: ")
+# Don't change the code above
+
+c = a   # Store value of A1 into new value, C
+a = b   # Assign value of B1 to A
+b = c   # Assign value of C(A1) to B
+
+
+# Don't change the code below
+print("a: " + a)
+print("b: " + b)
+# Don't change the code above
