92a82ebfc98f15984786ff5799a5cc8f42fa36a3
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-23 13:00:43 -0800

    Adding Day2Exercise2

---
+++ Day2/Day2Exerice2_BMICalculator.py
@@ -0,0 +1,10 @@
+
+# More readable
+#height = float(input("enter your height in m: "))
+#weight = float(input("enter your weight in kg: "))
+
+#print(int((weight / height ** 2)))
+
+
+#One line
+print(int((float(input("enter your weight in kg: ")) / float(input("enter your height in m: ")) ** 2)))
