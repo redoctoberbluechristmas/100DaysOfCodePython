78e57434abb02cd31fa39e92405e5a3246d231d8
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-23 16:00:21 -0800

    Finding alternate solutions to Exercise 3

--- Day2/Day2Exercise3_TipCalculator.py
+++ Day2/Day2Exercise3_TipCalculator.py
@@ -1,10 +1,24 @@
-from decimal import Decimal
-
 print("Welcome to the tip calculator.")
 total_bill = float(input("What is the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100 * total_bill
 number_of_diners = int(input("How many people to split the bill? "))
 
-individual_share = round(Decimal(str((total_bill + tip) / number_of_diners)), 2)
+#formating to get two decimal places when rounded.
+individual_share = "{:.2f}".format((total_bill + tip) / number_of_diners)
 
 print(f"Each person should pay: $ {individual_share}")
+
+
+
+#Alternate way to resolve individual_share display issue with casting.
+
+# from decimal import Decimal
+
+# print("Welcome to the tip calculator.")
+# total_bill = float(input("What is the total bill? $"))
+# tip = int(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100 * total_bill
+# number_of_diners = int(input("How many people to split the bill? "))
+
+# individual_share = round(Decimal(str((total_bill + tip) / number_of_diners)), 2)
+
+# print(f"Each person should pay: $ {individual_share}")
