78dd5fc208185398ab1d99ca0db850c8d3cc7abb
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-24 16:17:19 -0800

    Adding some primitive/imperfect input validation.

--- Day3/Day3Exercise6_TreasureIsland.py
+++ Day3/Day3Exercise6_TreasureIsland.py
@@ -23,18 +23,25 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-left_or_right = (input("Would you like to go left or right?\n")).lower()
+#This will convert input to lower, and look for anything like "left"
+left_or_right = ((input("Would you like to go left or right?\n")).lower()).find("left")
 
-if(left_or_right == "left"):
+# If left_or_right does not NOT (!= -1) contain "left", you turn left. If it comes back 0, you turn not left (or right)
+if(left_or_right != -1):
+  
   print("You turn left. You're a beach. There's an island in the distance, but it's pretty far and the water looks rough.")
-  swim_or_wait = (input("Would you like to swim across or wait?\n")).lower()
-  if(swim_or_wait == "wait"):
+  swim_or_wait = ((input("Would you like to swim across or wait?\n")).lower()).find("wait")
+    
+    if(swim_or_wait != -1):
+    
     print("You decide to wait. Eventually, a boat appears and takes you to the island. On the island, you find a wall with three doors. One is red, one is yellow, and one is blue.")
-    which_door = (input("Which door do you choose? Red, Yellow, or Blue?\n")).lower()
-    if(which_door == "yellow"):
+    which_door = ((input("Which door do you choose? Red, Yellow, or Blue?\n")).lower()).find("yellow")
+    
+    if(which_door != -1):
       print("There is a treasure behind the yellow door. You win!")
     else:
       print("Sorry, you triggered a trap. Game Over!")
+  
   else:
     print("You tried to swim, but ran out of energy and died. Game over.")
 else:
