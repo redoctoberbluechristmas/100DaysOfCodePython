03e91c5f246bd1c8eac0d3613fa4c799f5520b33
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-25 13:13:03 -0800

    refactor

--- Day4/Day4Exercise3_TreasureMap.py
+++ Day4/Day4Exercise3_TreasureMap.py
@@ -9,21 +9,17 @@ position = input("Where do you want to put the treasure? ")
 
 #Write your code below this row 👇
 
-# Convert string input into simple, 2 index list.
 inputindices = list(position)
 
-# Cast input to int, so can be referenced in map below. 
-# Updated with -1, so the input could follow the more natural 
-# "Row 1, Column 1 is upper left corner" pattern,
-# as opposed to starting with 0s. 
-column_coord = (int(inputindices[0]) -1)
-row_coord = (int(inputindices[1]) - 1)
+print(f"These are the {inputindices}")
 
-#Logging.
+row_coord = (int(inputindices[0]) - 1)
+column_coord = (int(inputindices[1]) -1)
+
 print(f"{row_coord} is row.")
 print(f"{column_coord} is the column.")
 
-map[row_coord][column_coord] = "X"
+map[column_coord][row_coord] = "X"
 
 
 #Write your code above this row 👆
