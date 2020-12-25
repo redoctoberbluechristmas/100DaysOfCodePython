ac1bc43f25da4f3c96f61f4162e98eb4be59396b
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-24 16:22:19 -0800

    updating left/right text

--- Day3/Day3Exercise6_TreasureIsland.py
+++ Day3/Day3Exercise6_TreasureIsland.py
@@ -24,25 +24,23 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
 #This will convert input to lower, and look for anything like "left"
-left_or_right = ((input("Would you like to go left or right?\n")).lower()).find("left")
+left_or_right = ((input('You\'re at a crossroads. Would you like to go "left" or "right"?\n')).lower()).find("left")
 
 # If left_or_right does not NOT (!= -1) contain "left", you turn left. If it comes back 0, you turn not left (or right)
 if(left_or_right != -1):
-  
   print("You turn left. You're a beach. There's an island in the distance, but it's pretty far and the water looks rough.")
   swim_or_wait = ((input("Would you like to swim across or wait?\n")).lower()).find("wait")
-    
-    if(swim_or_wait != -1):
-    
+  if(swim_or_wait != -1):
     print("You decide to wait. Eventually, a boat appears and takes you to the island. On the island, you find a wall with three doors. One is red, one is yellow, and one is blue.")
     which_door = ((input("Which door do you choose? Red, Yellow, or Blue?\n")).lower()).find("yellow")
-    
     if(which_door != -1):
       print("There is a treasure behind the yellow door. You win!")
     else:
       print("Sorry, you triggered a trap. Game Over!")
-  
   else:
     print("You tried to swim, but ran out of energy and died. Game over.")
 else:
   print("Some lion ate you, game over.")
+
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
