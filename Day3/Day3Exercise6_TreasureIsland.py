b210b12fbf27ab27add0de4322e9dc407fda48e3
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-24 16:40:09 -0800

    remove link to flowchart

--- Day3/Day3Exercise6_TreasureIsland.py
+++ Day3/Day3Exercise6_TreasureIsland.py
@@ -41,6 +41,3 @@ if(left_or_right != -1):
     print("You tried to swim, but ran out of energy and died. Game over.")
 else:
   print("Some lion ate you, game over.")
-
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
