8b8875ee020f33c6b944b9f2620824e022165cec
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-25 15:28:45 -0800

    Refactoring RPS game

--- Day4/Day4Exercise4_RockPaperScissors.py
+++ Day4/Day4Exercise4_RockPaperScissors.py
@@ -30,41 +30,25 @@ import random
 
 player_choice = input("What do you choose? Rock, paper, or scissors?  ").lower()
 
-print(player_choice)
+valid_choices = ["rock", "paper", "scissors"]
 
-if player_choice.startswith("r"):
-  player_choice = 0
-  print("You chose rock.")
-elif player_choice.startswith("p"):
-  player_choice = 1
-  print("You chose paper.")
-elif player_choice.startswith("s"):
-  player_choice = 2
-  print("You chose scissors.")
+if player_choice not in valid_choices:
+  print("Please enter a valid choice.")
 else:
-  print("Please make a valid choice.")
-  exit()
+  print(f"You chose {player_choice}")
+  player_choice = valid_choices.index(player_choice)
+  print(game_images[player_choice])
 
-#Print picture based on player choice.
-print(game_images[player_choice])
+  computer_choice = random.randint(0, 2)
+  computer_choice_output = valid_choices[computer_choice]
+  print(f"The computer chose {computer_choice_output}")
+  print(game_images[computer_choice])
 
-computer_choice = random.randint(0, 2)
-
-if(computer_choice == 0):
-  print("The computer chose rock.")
-elif(computer_choice == 1):
-  print("The computer chose paper.")
-else:
-  print("The computer chose scissors.")
-
-#Print picture for computer choice.
-print(game_images[computer_choice])
-
-if player_choice == 0 and computer_choice == 2:
-  print("You win!")
-elif player_choice > computer_choice:
-  print("You win!")
-elif player_choice == computer_choice:
-  print("You tie.")
-else:
-  print("Computer wins.")
+  if player_choice == 0 and computer_choice == 2:
+    print("You win!")
+  elif player_choice > computer_choice:
+    print("You win!")
+  elif player_choice == computer_choice:
+    print("You tie.")
+  else:
+    print("Computer wins.")
