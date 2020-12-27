f48b1f3048aafc8ef0b1f9f4fb5344e0a29e2c1a
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-27 10:43:16 -0800

    add comments

--- Day6/Day6Exercise2_ReeborgHurdlesChallenge.py
+++ Day6/Day6Exercise2_ReeborgHurdlesChallenge.py
@@ -4,7 +4,9 @@
 def turn_right():
     for i in range(0, 3):
         turn_left()
-    
+
+# This isn't necessary to meet the challenge, especially given the challenge's short length, it just bothered me when I
+# saw repeatable steps that weren't reduced to a function.
 def move_left():
     move()
     turn_left()
