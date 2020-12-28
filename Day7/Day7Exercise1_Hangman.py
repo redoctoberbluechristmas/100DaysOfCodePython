b227464272222e4f42db11622a35e5e6f8e2767d
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-28 15:34:04 -0800

    Add hangman game

--- Day7/Day7Exercise1_Hangman.py
+++ Day7/Day7Exercise1_Hangman.py
@@ -1,47 +1,102 @@
 import random
 
-### Generate a random word ###
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 word_list = ["aardvark", "baboon", "camel"]
+chosen_word = list(random.choice(word_list))
 
-
-chosen_word = word_list[random.randint(0, len(word_list)-1)]
-
-print(chosen_word)
-
-
-### Generate as many blanks as there are spaces in word ###
-
-
-
-
+blanks_list = []
+for i in range(len(chosen_word)):
+    blanks_list += "_"
 
 ### Prompt player for letter ###
 
+end_of_game = False
 
+lives = 6
 
+while not end_of_game:
+    guess = input("Please guess a letter: ").lower()
 
+    for i in range(len(chosen_word)):
+        #letter = value at chosen_word[0], letter = value at chosen_word[1], etc.
+        letter = chosen_word[i]
+        if letter == guess:
+            print("Match")
+            blanks_list[i] = letter
 
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You have {lives} lives left")
 
+    print(stages[lives])
 
+    blanks_list_string = ""
 
-### Replace the blank with a letter ###
+    for i in blanks_list:
+        blanks_list_string += f" {i} "
 
+    print(f"The current panel is:\n {blanks_list_string}")
 
+    if '_' not in blanks_list:
+        end_of_game = True
+        print("You win.")
 
-#### Check if all blanks are filled ####
-
-
-
-##### If no, return to prompt. If yes, print Game Over #####
-
-
-
-### Lose a life ###
-
-
-#### Check if run out of lives ####
-
-
-
-##### If no, return to prompt. If yes, print Game Over ####
+    if lives == 0:
+        end_of_game = True
+        print("You lose.")
