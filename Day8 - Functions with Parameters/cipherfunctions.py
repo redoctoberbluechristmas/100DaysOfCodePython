efa123086ba1100cef9ec9e91861af7b88916a0d
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2020-12-29 16:05:57 -0800

    doing things with input

--- Day8 - Functions with Parameters/cipherfunctions.py
+++ Day8 - Functions with Parameters/cipherfunctions.py
@@ -5,14 +5,16 @@ alphabet = [
 def caesar(start_text, shift_amount, cipher_direction):
     output_text = ""
     
-
     # Will divide the shift amount to fit into length of alphabet.
     shift_amount = shift_amount % 26
     
     if cipher_direction == "decode":
         shift_amount *= -1
     for char in start_text:
-        start_index = alphabet.index(char)
-        end_index = start_index + shift_amount
-        output_text += alphabet[end_index]
+        if char not in alphabet:
+            output_text += char
+        else:
+            start_index = alphabet.index(char)
+            end_index = start_index + shift_amount
+            output_text += alphabet[end_index]
     print(f"The {cipher_direction}d text is {output_text}.")
