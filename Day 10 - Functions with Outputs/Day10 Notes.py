ff703e1bfb4491cce03d0bc69e8e6f6d614cbcbe
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2021-01-05 08:50:23 -0800

    Add Day10Exercise1

--- Day 10 - Functions with Outputs/Day10 Notes.py
+++ Day 10 - Functions with Outputs/Day10 Notes.py
@@ -1,15 +1,35 @@
 #Functions with outputs
 
-f_name = input("What is your first name? ")
-l_name = input("What is your last name? ")
+# f_name = input("What is your first name? ")
+# l_name = input("What is your last name? ")
 
-def format_name(f_name, l_name):
-    f_name.lower()
-    f_name[0].upper()
+def format_name(first_name, last_name):
+    if first_name == "" or last_name == "":
+        return "Sorry, can't do it."
+    
+    formated_f_name = first_name.title()
+    formated_l_name = last_name.title()
 
-    l_name.lower()
-    f_name[0].upper()
+    # Use return keyword to specify what is returned by your function.
+    # Everything after return keyword will replace where the function was called.
+    return f"{formated_f_name} {formated_l_name}"
 
-normalized_names = format_name
 
-print(normalized_names)
+# Save the output of your function call in a variable.
+# output_storing_variable = format_name(f_name, l_name)
+output_storing_variable = format_name(input("What is your first name? "), input("What is your last name? "))
+print(output_storing_variable)
+
+OR
+# 
+#print(format_name(f_name, l_name))
+
+#Var, which stores output    function, returns output           input
+output =                      len                            ( "Hello" )
+
+
+
+
+#Multiple return values
+
+# return tells function that this is the end of function.
