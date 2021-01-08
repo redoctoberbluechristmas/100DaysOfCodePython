4bc2072253c0b10bd5f79b036c2cc982d419cb90
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2021-01-07 21:48:01 -0800

    Early version of blackjack game

--- Day11 - Blackjack Capstone.py
+++ Day11 - Blackjack Capstone.py
@@ -3,52 +3,72 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def get_starting_hands():
-    for i in range (2):
-        player_hand.append(random.choice(cards))
-        #computer_hand.append(random.choice(cards))
+def deal_card(somehand):
+    somehand.append(random.choice(cards))
 
-
-def hit():
-    player_hand.append(random.choice(cards))
-
-def calculate_score():
+def calculate_score(somehand):
     total = 0
-    for i in player_hand:
+    for i in somehand:
         total += i
     return total
 
-calculate_score()
+
+def get_starting_hands():
+    for i in range (2):
+        deal_card(player_hand)
+        deal_card(computer_hand)
+
+def running_total(somecalcscore):
+    return
+
 
 more_blackjack = True
 while more_blackjack:
 
-    # Get starting hands for player and computer.
     player_hand = []
     computer_hand = []
 
     get_starting_hands()
 
-    print(f"Your starting hand is {player_hand}")
-    print(f"The computer's starting hand is {computer_hand}")
-    
-
-    # Ask if player will hit or stand:
-    playing = True
-    
-    while playing == True:
-        if (input("Please enter 'hit' or 'stand': ")) == 'hit':
-            hit()
-            print(f"Your hand is {player_hand}")
-            calculate_score()
+    player_continue = True
+    while player_continue:
+        print(f"Your hand is {player_hand}")
+        if input("Press 'hit' or 'stand' ") == 'hit':
+            deal_card(player_hand)
+            player_total = calculate_score(player_hand)
+            if player_total > 21:
+                print(f"You busted with a score of {player_total}, you lose.")
+                player_continue = False
         else:
-            playing = False
-    
+            print("Player stands.")
+            player_continue = False
 
-    #Computer's Logic
-        # If total of computer hand is less than 17, hit.
+    player_total = calculate_score(player_hand)   
 
+    computer_total = calculate_score(computer_hand)
 
+    while computer_total < 21 and computer_total < player_total:
+        deal_card(computer_hand)
+        computer_total += calculate_score(computer_hand)
+
+    print(f"Your total is {player_total}")
+    print(f"Dealer total is {computer_total}")
+
+    if computer_total == 21:
+        print("Dealer blackjack, you lose.")
+    elif computer_total == player_total:
+        print("Tie, dealer wins.")
+    elif computer_total > 21:
+        print("Dealer busted, you win.")
+    elif computer_total > player_total:
+        print(f"Dealer score is {computer_total}, your score is {player_total}. You lose.")
+
+    # while computer_total < 21 and computer_total < player_total:
+    #     deal_card(computer_hand)
+    #     if computer_total == 21:
+    #         print("Dealer blackjack, you lose.")
+    #     elif computer_total > 21:
+    #         print("Dealer bust, you lose.")
 
 
     # Logic to keep playing.
