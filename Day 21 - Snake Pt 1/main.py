068e7c9f19c37c16f39f2450e0283473b3633568
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2021-02-10 07:47:32 -0800

    Updating snake game

--- Day 21 - Snake Pt 1/main.py
+++ Day 21 - Snake Pt 1/main.py
@@ -11,6 +11,13 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
 
 screen.update()
 
--- Day 21 - Snake Pt 1/snake.py
+++ Day 21 - Snake Pt 1/snake.py
@@ -1,20 +1,38 @@
-
 from turtle import Turtle
+
+# In Python, constants are spelt with all caps, snake case.
+# Reason to have constants is so that you don't have to dig through code to make minor changes.
+# These are things which will remain the same NO MATTER HOW MANY OBJECTS you produce.
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
-        snake_body = []
+        self.snake_body = []
 
-    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-    snake_body = []
+        # Can call methods when you initialize object of class.
+        self.create_snake()
 
-    for i in starting_positions:
-        snake_body_segments = Turtle(shape="square")
-        snake_body_segments.color("white")
-        snake_body_segments.penup()
-        snake_body_segments.goto(i)
-        snake_body.append(snake_body_segments)
+        # Replacing self.snake_body[0] in code so I don't have to write it out every time.
+        # Make sure to put self.head below self.snake_body and self.create_snake(), because this attribute references
+        # snake_body, and won't exist prior to those two.
+
+        self.head = self.snake_body[0]
+
+    def create_snake(self):
+
+        for i in STARTING_POSITIONS:
+            snake_body_segments = Turtle(shape="square")
+            snake_body_segments.color("white")
+            snake_body_segments.penup()
+            snake_body_segments.goto(i)
+            self.snake_body.append(snake_body_segments)
 
     def move(self):
 
@@ -22,5 +40,21 @@ class Snake:
             new_x = self.snake_body[seg_num - 1].xcor()
             new_y = self.snake_body[seg_num - 1].ycor()
             self.snake_body[seg_num].goto(new_x, new_y)
-        # Need to continue to propel the first segment, outside of the loop, for the snake to move forward.
-        self.snake_body[0].forward(20)
+        # Need to continue to propel the first segment, outside of the for loop, for the snake to move forward.
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
