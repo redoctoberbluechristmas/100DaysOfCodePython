da85c2769c93ae02924704b2e5013a64714054f2
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2021-03-02 09:04:42 -0800

    pandas notes

--- Day 25 - Working with CSV/main.py
+++ Day 25 - Working with CSV/main.py
@@ -16,5 +16,70 @@
 
 import pandas
 
+# doing typechecks on new library
+
+
+
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(type(data))   # returns pandas.core.frame.DataFrame (a DataFrame object)
+# # Two primary data structures of pandas; Series (1-dimensional) and DataFrame (2-dimensional)
+# print(type(data["temp"])) # returns pandas.core.series.Series; similare to a list
+# # The column "temp" is a series.
+
+#data_dict = data.to_dict()
+#print(data_dict)
+
+# Average a list in conventional way.
+
+# Get data in columns
+
+# print(data["condition"])
+# print(data.condition)
+
+# Send series to list
+# temps = data["temp"].to_list()
+# average = sum(temps) / len(temps)
+# print(round(average, 2))
+
+# Use Pandas to get mean.
+
+#mean_temp = data.temp.mean()
+#print(round(mean_temp, 2))
+
+# Get ahold of maximum value using data series methods
+
+#max_value = data["temp"].max()
+#print(max_value)
+
+# Get data from rows in DataFrame
+
+#monday = data[data.day == "Monday"]   # Within the 'data' DataFrame, and within the column 'day', find rows == to "Monday"
+#print(monday)
+
+# Challenge - get row of data where temp was at max
+
+# max_temp = data.temp.max()
+# print(type(max_temp))
+# print(max_temp)
+# # It doesn't like it when you pass max_temp as variable
+# print(data[data.temp == max_temp])  #<---doesn't work
+# print(data[data.temp == data.temp.max()])  #<--- works
+
+# Convert temp from Celsius to Fahrenheit
+# saturday = data[data.day == "Saturday"]
+#
+# # Convert Saturday temp type to normal value, int
+# fahrenheit_temp = int(saturday.temp) * 9 / 5 + 32
+# print(fahrenheit_temp)
+
+
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+# Create a dataframe from scratch (not reading from csv)
+data = pandas.DataFrame(data_dict)
+
+# port dict to csv file.
+data.to_csv("new_data.csv")
