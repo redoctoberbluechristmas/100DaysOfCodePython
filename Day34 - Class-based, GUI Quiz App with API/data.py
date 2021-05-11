a6ee708223c7efb2e5a9f476aac24ef36e91c2cd
redoctoberbluechristmas <redoctoberbluechristmas@protonmail.com>
2021-05-11 08:31:42 -0700

    Change quiz to gui

---
+++ Day34 - Class-based, GUI Quiz App with API/data.py
@@ -0,0 +1,17 @@
+import requests
+
+# Target URI looks like this (reference when framing parameters): https://opentdb.com/api.php?amount=10&type=boolean
+
+QUESTION_AMOUNT = 10
+QUESTION_TYPE = "boolean"
+
+request_parameters = {
+    "amount": QUESTION_AMOUNT,
+    "type": QUESTION_TYPE,
+}
+
+response = requests.get(f"https://opentdb.com/api.php", params=request_parameters)
+response.raise_for_status()
+data = response.json()
+
+question_data = data['results']
